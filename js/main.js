0ef61bb612f5d880c9500dee5c7ed0de7e9e9a5a
lilitantonyan <lilit@helix.am>
2024-06-23 15:21:54 +0400

    add project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.banner').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 1,
+        dots: false
+    })
+    $('.top-sale-list').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 4,
+        dots: false
+    })
+})
